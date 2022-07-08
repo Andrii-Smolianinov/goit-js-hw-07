0d80a2631f922b6073dd7686f7aa3637d1108227
Andrii-Smolianinov <andr380v@gmail.com>
2022-07-08 07:54:21 +0300

    homework 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const GalleryEl = document.querySelector(".gallery");
+const cardMarcup = createElementGalery(galleryItems);
 
+GalleryEl.insertAdjacentHTML("beforeend", cardMarcup);
+
+function createElementGalery(galleryItems) {
+  return galleryItems.reduce(
+    (acc, { description, original, preview }) =>
+      acc +
+      `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+    </a>
+    </div>`,
+    ""
+  );
+}
+
+GalleryEl.addEventListener("click", onImageClick);
+function onImageClick(e) {
+  e.preventDefault();
+  
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  //*отримання url оригінального зображення.
+  const originalImage = e.target.getAttribute("data-source");
+
+  //*Підключення скрипту і стилів бібліотеки модального вікна
+  const instance = basicLightbox.create(
+    `<img src="${originalImage}">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onESCCloseModal);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onESCCloseModal);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onESCCloseModal(e) {
+    if (e.key === "Escape") {
+      instance.close();
+      return;
+    }
+  }
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const GalleryEl = document.querySelector(".gallery");
+const cardMarcup = createElementGalery(galleryItems);
 
-console.log(galleryItems);
+GalleryEl.insertAdjacentHTML("beforeend", cardMarcup);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionDelay: 250,
+  captionsData: "alt",
+  close: false,
+  overlayOpacity: 0.8,
+  fadeSpeed: 200,
+});
+
+function createElementGalery(galleryItems) {
+  return galleryItems.reduce(
+    (acc, { description, original, preview }) =>
+      acc +
+      `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+    `,
+    ""
+  );
+}
+
+
+
+
